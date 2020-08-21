546116cff9a1776177832359cebd71e5e5b7b774
Michelle Wong <michellekwong2@gmail.com>
2020-08-21 15:46:02 -0400

    Added items to json file & CSS layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
-
 import json from './items.json'
+import Card from './card'
 
 function App() {
   console.log(json.items)
   return (
     <div className="App">
      <h1>Retail Me</h1>
-     <h2></h2>
+     <div className="flex-container">
+       {
+         json.items.map(item => <Card {...item} key={item.id} /> )
+       }
+     </div>
     </div>
   );
 }
--- src/card.js
+++ src/card.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export default function Card(){
+export default function Card(props){
   return(
-    <div>
-      <h1>Card name</h1>
+    <div className="item">
+      <img src={props.front.image} />
+      <h1>{props.name}</h1>
+      <h1>{props.price}</h1>
     </div>
   )
 }
