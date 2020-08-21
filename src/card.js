d3225af10555a2149a1e81753f3ad9f0db45f1b7
Michelle Wong <michellekwong2@gmail.com>
2020-08-21 16:10:25 -0400

    On hover, image flips

--- src/card.js
+++ src/card.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function Card(props){
+  const front = props.front.image
+  const[src, setSrc] = useState(front)
+
   return(
     <div className="item">
-      <img src={props.front.image} />
+      <img src={src} alt="front" onMouseLeave={() => setSrc(props.front.image)} onMouseOver={() => setSrc(props.back.image)}/>
       <h1>{props.name}</h1>
       <h1>{props.price}</h1>
     </div>
