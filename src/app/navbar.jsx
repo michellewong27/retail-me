ce237d19b2fceb5bc03531ac4b17d309881ddddc
Michelle Wong <michellekwong2@gmail.com>
2020-08-31 15:38:37 -0400

    Navbar css layout

--- src/app/navbar.jsx
+++ src/app/navbar.jsx
@@ -3,34 +3,32 @@ import { NavLink } from "react-router-dom";
 
 export default function Navbar(){
   return (
-    <div className="navbar">
-      <ul>
-        <li>
-          <NavLink to="/" exact activeStyle={{ color: "black" }}>
-            Retail Me Logo
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/new" exact activeStyle={{ color: "black" }}>
-            New
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/clothing" exact activeStyle={{ color: "black" }}>
-            Clothing
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/shoes" exact activeStyle={{ color: "black" }}>
-            Shoes
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/cart" exact activeStyle={{ color: "black" }}>
-            My Cart
-          </NavLink>
-        </li>
-      </ul>
-    </div>
+    <ul className="navbar">
+      <li>
+        <NavLink to="/" exact activeStyle={{ color: "gray" }}>
+          Retail Me Logo
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/new" exact activeStyle={{ color: "gray" }}>
+          New
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/clothing" exact activeStyle={{ color: "gray" }}>
+          Clothing
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/shoes" exact activeStyle={{ color: "gray" }}>
+          Shoes
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/cart" exact activeStyle={{ color: "gray" }}>
+          My Cart
+        </NavLink>
+      </li>
+    </ul>
   );
 }
