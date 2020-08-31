ce62eede41906ded90ba858a7b0c2116001d88a5
Michelle Wong <michellekwong2@gmail.com>
2020-08-31 15:28:39 -0400

    Add react-router

--- src/app/card/index.jsx
+++ src/app/card/index.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useRef, useEffect } from "react";
+import Item from '../item';
+// import {Link} from 'react-router-dom';
 
 export default function Card(props) {
   const front = props.front.image;
   const [src, setSrc] = useState(front);
   const [ref, hovered] = useHover();
-  // const [button, setButton] = useState()
 
-  //on hover, set src to be back & button to appear
   function useHover() {
     const ref = useRef(null);
     const [hovered, setHovered] = useState(false);
@@ -27,9 +27,10 @@ export default function Card(props) {
     return [ref, hovered];
   }
 
-  function viewPage() {
-    console.log("Viewing page", props);
-    
+  function viewItem(e) {
+    console.log("Viewing item page", props);
+    e.preventDefault();
+
   }
 
   function addToCart() {
@@ -39,37 +40,39 @@ export default function Card(props) {
   //onClick of add to cart, also views product page?
 
   return (
-    <div
-      className="card"
-      ref={ref}
-      onMouseLeave={() => setSrc(props.front.image)}
-      onMouseEnter={() => setSrc(props.back.image)}
-      onClick={viewPage}
-    >
-      <div className="image-wrap">
-        <img src={src} alt="front" />
-        {hovered && (
-          <div className="quick-add">
-            <button className="button primary">Quick view</button>
-            <div className="select-size">
-              <select className="select-btn" name="Size" id="size">
-                <option value="XS">XS</option>
-                <option value="S">S</option>
-                <option value="M">M</option>
-                <option value="L">L</option>
-              </select>
+    // <Link to='/clothing/:id'>
+      <div
+        className="card"
+        ref={ref}
+        onMouseLeave={() => setSrc(props.front.image)}
+        onMouseEnter={() => setSrc(props.back.image)}
+        onClick={(e) => viewItem(e)}
+      >
+        <div className="image-wrap">
+          <img src={src} alt="front" />
+          {hovered && (
+            <div className="quick-add">
+              <button className="button primary">Quick view</button>
+              <div className="select-size">
+                <select className="select-btn" name="Size" id="size">
+                  <option value="XS">XS</option>
+                  <option value="S">S</option>
+                  <option value="M">M</option>
+                  <option value="L">L</option>
+                </select>
+              </div>
+              <div className="addtocart">
+                <button className="button primary" onClick={addToCart}>
+                  Add to cart
+                </button>
+              </div>
             </div>
-            <div className="addtocart">
-              <button className="button primary" onClick={addToCart}>
-                Add to cart
-              </button>
-            </div>
-          </div>
-        )}
-      </div>
+          )}
+        </div>
 
-      <h1>{props.name}</h1>
-      <h1>${props.price}</h1>
-    </div>
+        <h1>{props.name}</h1>
+        <h1>${props.price}</h1>
+      </div>
+    // </Link>
   );
 }
