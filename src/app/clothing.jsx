5d51f1971e9705a7da9644fd58a6b2974c4e1f51
Michelle Wong <michellekwong2@gmail.com>
2020-09-10 20:46:09 -0400

    Clothing items render to clothing page

--- src/app/clothing.jsx
+++ src/app/clothing.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import json from "./carousel/items.json";
+import Card from "./card/index.jsx";
 
 export default function Clothing(){
-  
   return(
     <div className="clothing">
       <h1>All clothing items</h1>
+      {
+        json.items.map((item) => (
+          <Card {...item} key={item.id} />
+        ))
+      }
     </div>
   )
 }
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -3,7 +3,6 @@ import Navbar from './navbar';
 import Clothing from './clothing';
 import Item from './item';
 import { Route, Switch } from 'react-router-dom';
-
 import Carousel from "./carousel";
 
 export default function App() {
